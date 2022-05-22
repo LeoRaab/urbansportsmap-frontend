f9643072e72a76d257f4d04b87a0d00fd60a7248
LeoRaab <leopold.raab@gmail.com>
2022-05-22 11:08:41 +0200

    updated formatting

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,19 +1,21 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
 import Filter from '../components/Filter/Filter';
 import PageHeader from '../components/UI/PageHeader';
 import FavoritesList from '../components/Favorites/FavoritesList';
+import Modal from '../components/UI/Modal';
 import GraphicMessage from '../components/UI/GraphicMessage';
-import {ILLUSTRATIONS} from '../constants/Illustrations';
-import {useSelector} from 'react-redux';
-import {selectFilters} from '../store/filterSlice';
+import { ILLUSTRATIONS } from '../constants/Illustrations';
+import { selectFilters } from '../store/filterSlice';
 import LoadingSpinner from '../components/UI/LoadingSpinner';
-import {useGetFavoritesQuery} from '../store/api/favoritesApi';
+import { useGetFavoritesQuery } from '../store/api/favoritesApi';
 import Venue from '../types/Venue';
-import {hasVenueSportType} from '../util/has-venue-sport-type';
+import { hasVenueSportType } from '../util/has-venue-sport-type';
 
 const Favorites = () => {
 
-    const {data: favoriteVenues, isLoading, isFetching} = useGetFavoritesQuery();
+    const { data: favoriteVenues, isLoading, isFetching } = useGetFavoritesQuery();
     const filters = useSelector(selectFilters);
     const [filteredVenues, setFilteredVenues] = useState<Venue[]>();
 
@@ -37,22 +39,22 @@ const Favorites = () => {
                 <LoadingSpinner/>
             }
 
-            <PageHeader text={'Favoriten'}/>
+            <PageHeader text={'Favoriten'} />
 
             {(!isLoading && favoriteVenues && favoriteVenues.length === 0) &&
                 <GraphicMessage illustration={ILLUSTRATIONS.NOT_FOUND}
-                                title={'Keine Favoriten gefunden'}
-                                text={'Du hast noch keine Favoriten gespeichert.'}/>
+                    title={'Keine Favoriten gefunden'}
+                    text={'Du hast noch keine Favoriten gespeichert.'} />
             }
 
             {favoriteVenues && favoriteVenues.length > 0 &&
                 <>
                     <div className="px-2 my-6">
-                        <Filter/>
+                        <Filter />
                     </div>
 
                     <div className="flex flex-col gap-y-2 mt-4 px-2">
-                        <FavoritesList favoriteVenues={filteredVenues}/>
+                        <FavoritesList favoriteVenues={filteredVenues} />
                     </div>
                 </>
             }
