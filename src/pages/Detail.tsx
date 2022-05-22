ae6785a35f1eb6f453860f604188e34571d4c952
LeoRaab <leopold.raab@gmail.com>
2022-05-22 12:37:13 +0200

    add useEffect for loading Venue & Comments lazy

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -2,26 +2,43 @@
  * TODO: Show detail settings only when logged in
  */
 
-import React, {useState} from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import PageHeader from '../components/UI/PageHeader';
 import SportTypesList from '../components/SportTypesList/SportTypesList';
-import {useNavigate, useParams} from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import DetailSettings from '../components/Details/DetailSettings';
 import FabButton from '../components/UI/buttons/FabButton';
 import Icon from '../components/UI/Icon';
-import {ICONS} from '../constants/Icons';
+import { ICONS } from '../constants/Icons';
 import VenueTitle from '../components/UI/VenueTitle';
 import CommentForm from '../components/Forms/CommentForm';
 import Venue from '../types/Venue';
-import {useGetVenueByIdQuery} from '../store/api/venuesApi';
+import { useLazyGetVenueByIdQuery } from '../store/api/venuesApi';
+import Modal from '../components/UI/Modal';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+import VenueCommentsList from '../components/Details/VenueCommentsList';
+import { useAddCommentMutation, useLazyGetCommentsQuery } from '../store/api/commentsApi';
+import { useSelector } from 'react-redux';
+import { selectUserId } from '../store/authSlice';
 
 const Detail = () => {
 
     const navigate = useNavigate();
     const params = useParams();
+    const [loadVenue, { data: venue, isLoading, isFetching }] = useLazyGetVenueByIdQuery();
+    const [loadVenueComments, { data: venueComments }] = useLazyGetCommentsQuery();
+    const [addComment] = useAddCommentMutation();
+    const userId = useSelector(selectUserId);
+
     const [showCommentForm, setShowCommentForm] = useState<boolean>(false);
 
-    const {data: venue, isLoading, isFetching} = useGetVenueByIdQuery(params.venueId!);
+    useEffect(() => {
+        if (params.venueId) {
+            loadVenue(params.venueId);
+            loadVenueComments(params.venueId);
+            console.log(venueComments);
+        }
+    }, [params.venueId])
 
     const handleEditImagesClick = () => {
         //venueImages.loadImages();
@@ -31,16 +48,8 @@ const Detail = () => {
         setShowCommentForm(true);
     }
 
-    const handleEditCommentClick = (comment: string, commentId: number) => {
-        //venueComments.edit(comment, commentId);
-    }
-
-    const handleDeleteCommentClick = (commentId: number) => {
-        //venueComments.remove(commentId);
-    }
-
     const handleFormSubmit = (comment: string) => {
-        //venueComments.add(comment);
+        addComment({ venueId: venue!.id, comment });
         setShowCommentForm(false);
     }
 
@@ -49,52 +58,56 @@ const Detail = () => {
     }
 
     return (
-        <div className="h-screen relative">
-
-            <PageHeader text={'Detail'}/>
-
-            <div className="mt-6">
-                <VenueTitle venue={venue}/>
-            </div>
-
-            <div className="px-2 mt-6 mb-2">
-                <SportTypesList sportTypes={venue?.sportTypes}/>
-            </div>
-
-            {venue?.id &&
-                <DetailSettings venue={venue}
-                                onEditImagesClick={handleEditImagesClick}
-                                onCommentClick={handleAddCommentClick}/>
+        <>
+            {(isLoading || isFetching) &&
+                <LoadingSpinner />
             }
+            <div className="h-screen relative">
 
-            {showCommentForm &&
-                <CommentForm onFormSubmit={handleFormSubmit} onFormCancel={handleFormCancel}/>
-            }
+                <PageHeader text={'Detail'} />
 
-            {/*
-                <div className="h-1/3 my-4">
-                    <ImageSwiper images={venueImages.images}/>
+                <div className="mt-6">
+                    <VenueTitle venue={venue} />
                 </div>
-            */}
+
+                <div className="px-2 mt-6 mb-2">
+                    <SportTypesList sportTypes={venue?.sportTypes} />
+                </div>
+
+                {(userId && venue?.id) &&
+                    <DetailSettings venue={venue}
+                        onEditImagesClick={handleEditImagesClick}
+                        onCommentClick={handleAddCommentClick} />
+                }
+
+                {showCommentForm &&
+                    <CommentForm onFormSubmit={handleFormSubmit} onFormCancel={handleFormCancel} />
+                }
+
+                {/*
+    <div className="h-1/3 my-4">
+        <ImageSwiper images={venueImages.images}/>
+    </div>
+*/}
 
 
-            <div className="fixed bottom-6 right-2 z-800">
-                <FabButton backgroundColor="bg-green-200"
-                           onFabButtonClick={() => navigate('/' + venue?.location.lat + ',' + venue?.location.lng)}>
-                    <Icon icon={ICONS.MAP}/>
-                </FabButton>
+                <div className="fixed bottom-6 right-2 z-800">
+                    <FabButton backgroundColor="bg-green-200"
+                        onFabButtonClick={() => navigate('/' + venue?.location.lat + ',' + venue?.location.lng)}>
+                        <Icon icon={ICONS.MAP} />
+                    </FabButton>
+                </div>
+
+                {(venueComments && venueComments.length > 0) &&
+                    <div className="px-2 mt-4 mb-4">
+                        <h2 className="text-2xl mb-4">Kommentare</h2>
+                        <VenueCommentsList comments={venueComments} />
+                    </div>
+                }
+
             </div>
+        </>
 
-            {/*venueComments.comments.length > 0 &&
-                <div className="px-2 mt-4 mb-4">
-                    <h2 className="text-2xl mb-4">Kommentare</h2>
-                    <VenueCommentsList comments={venueComments.comments}
-                                       onEditClick={handleEditCommentClick}
-                                       onDeleteClick={handleDeleteCommentClick}/>
-                </div>
-            */}
-
-        </div>
     )
 }
 
