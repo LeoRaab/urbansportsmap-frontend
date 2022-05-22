efc09513331f80ff7e08b122cdeac57da7504cbe
LeoRaab <leopold.raab@gmail.com>
2022-05-22 10:56:00 +0200

    add requireauth to protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
 /**
- * TODO: Add loading indicator using useReducer action.type = pending, loading
- * TODO: Try and catch api calls and Error Handling
- * TODO: Fetch MapSettings from API
  * TODO: Button Components --> Styled Components
- * TODO: Improve input-range usability & design
  * TODO: Check color persistence
  * TODO: Add Toast component to display success and error
  * TODO: Add Dialog component for user actions
  * TODO: How to get fullscreen on browsers, its working when going to favorites and then back to map
- * TODO: Think about modal provider
  * TODO: Optimize for larger screens
  * TODO: Improve Button Component Structure + disabledVersion
  * TODO: Check if passing objectId as prop to ImagePicker is the best way
  * TODO: Implement Report Image/Comment Feature
- * TODO: Implement useFilter-Hook
- * TODO: Simplify useImagePicker-Hook
- * TODO: Timeout for opendata api call
- * TODO: Save & load VenueWebEntities to/from db
  * TODO: Close menu on mapClick
  * TODO: Check userInput @ forms
  * TODO: Check if username & email is already in use
@@ -25,12 +16,11 @@
  * TODO: Check rerender amount
  * TODO: Check z-indexes
  * TODO: Zoom in when clicking on search result
- * TODO: Find better solution for comparing sportTypes in Venue and MarkerIcon
  */
 
 import React from 'react';
 import Home from './pages/Home';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useLocation, Navigate } from 'react-router-dom';
 import Detail from './pages/Detail';
 import Favorites from './pages/Favorites';
 import MenuButton from './components/UI/buttons/MenuButton';
@@ -39,10 +29,9 @@ import Profile from './pages/Profile';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import PageNotFound from './pages/PageNotFound';
-import {useDispatch, useSelector} from 'react-redux';
-import {selectUi, uiActions} from './store/uiSlice';
-import {useGetVenuesQuery} from './store/api/venuesApi';
-import LoadingSpinner from './components/UI/LoadingSpinner';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectUi, uiActions } from './store/uiSlice';
+import { selectUserId } from './store/authSlice';
 
 const App = () => {
 
@@ -61,28 +50,47 @@ const App = () => {
         <BrowserRouter>
 
             <div className="fixed top-7 right-0 z-1000">
-                <MenuButton isShowing={ui.isMenuShowing} onMenuButtonClick={handleMenuButtonClick}/>
+                <MenuButton isShowing={ui.isMenuShowing} onMenuButtonClick={handleMenuButtonClick} />
             </div>
 
-            <Menu isShowing={ui.isMenuShowing}/>
+            <Menu isShowing={ui.isMenuShowing} />
 
             <main className="h-screen" onClick={handleMainClick}>
                 <Routes>
-                    <Route path="/" element={<Home/>}>
-                        <Route path="/location/:latLng" element={<Home/>}/>
+                    <Route path="/" element={<Home />}>
+                        <Route path="/location/:latLng" element={<Home />} />
                     </Route>
-                    <Route path="/favorites" element={<Favorites/>}/>
-                    <Route path="/detail" element={<Detail/>}>
-                        <Route path=":venueId" element={<Detail/>}/>
+                    <Route path="/detail" element={<Detail />}>
+                        <Route path=":venueId" element={<Detail />} />
                     </Route>
-                    <Route path="/profile" element={<Profile/>}/>
-                    <Route path="/login" element={<Login/>}/>
-                    <Route path="/signup" element={<Signup/>}/>
-                    <Route path="/*" element={<PageNotFound/>}/>
+                    <Route path="/favorites" element={
+                        <RequireAuth>
+                            <Favorites />
+                        </RequireAuth>
+                    } />
+                    <Route path="/profile" element={
+                        <RequireAuth>
+                            <Profile />
+                        </RequireAuth>
+                    } />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/*" element={<PageNotFound />} />
                 </Routes>
             </main>
         </BrowserRouter>
     );
+}
+
+const RequireAuth = ({ children }: { children: JSX.Element }) => {
+    const location = useLocation();
+    const userId = useSelector(selectUserId);
+    console.log(userId);
+    if (!userId) {
+        return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+
+    return children;
 }
 
 export default App;
