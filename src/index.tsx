f7ca23207b01741dc6c777ac8923ec8b4dc1edd9
LeoRaab <leopold.raab@gmail.com>
2022-05-22 11:03:33 +0200

    update react init method to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'leaflet/dist/leaflet.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {defineCustomElements} from '@ionic/pwa-elements/loader';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 import store from './store';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(
+let appRootNode = document.getElementById("app-root");
+
+if (!appRootNode) {
+    appRootNode = document.createElement('div');
+    appRootNode.id = 'app-root';
+    document.body.append(appRootNode);
+}
+
+
+const root = ReactDOM.createRoot(appRootNode);
+root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App/>
+            <App />
         </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
 
 defineCustomElements(window);
@@ -23,3 +32,5 @@ defineCustomElements(window);
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
