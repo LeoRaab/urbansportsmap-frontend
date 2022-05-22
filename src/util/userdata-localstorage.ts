a3ad39fd853c1853bece1cb8f88b546581e76a66
LeoRaab <leopold.raab@gmail.com>
2022-05-22 11:10:34 +0200

    removed isLoggedIn flag

--- src/util/userdata-localstorage.ts
+++ src/util/userdata-localstorage.ts
@@ -6,29 +6,32 @@ export const getUserDataFromStorage = (): AuthState => {
     if (userDataJSON) {
         try {
             const userData = JSON.parse(userDataJSON) as AuthState;
+            console.log(userData);
+            console.log(userData.userId);
             return {
                 userId: userData.userId,
-                token: userData.token,
-                isLoggedIn: true
+                token: userData.token
             }
         }
         catch (e) {
             return {
                 userId: null,
-                token: null,
-                isLoggedIn: false
+                token: null
             }
         }
     }
 
     return {
         userId: null,
-        token: null,
-        isLoggedIn: false
+        token: null
     }
 
 }
 
 export const removeUserDataFromStorage = (): void => {
     localStorage.removeItem('userData');
+}
+
+export const setUserDataToStorage = (userData: AuthState): void => {
+    localStorage.setItem('userData', JSON.stringify(userData));
 }
