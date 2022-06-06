296d7a99f8c0e8ca13441d9a5c0eac63092a62d2
LeoRaab <leopold.raab@gmail.com>
2022-06-06 14:35:32 +0200

    removed unneccesary imports

--- src/pages/main-layout.tsx
+++ src/pages/main-layout.tsx
@@ -1,24 +1,12 @@
-import React, { useState } from 'react';
-import Search from '../components/Search/Search';
-import FabButton from '../components/UI/buttons/FabButton';
-import Teaser from '../components/Teaser/Teaser';
-import Filter from '../components/Filter/Filter';
+import React from "react";
 import { Outlet, useParams } from "react-router-dom";
-import L from 'leaflet';
-import Icon from '../components/UI/Icon';
-import MapOverlay from '../components/Map/MapOverlay';
 import MapWrapper from '../components/Map/MapWrapper';
-import RangeSlider from '../components/UI/RangeSlider';
-import { ICONS } from '../constants/Icons';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectUi, uiActions } from '../store/uiSlice';
-import { mapCenterChanged, venueRadiusChanged } from '../store/mapSlice';
+import { mapCenterChanged } from '../store/mapSlice';
 
 const MainLayout = () => {
 
     const dispatch = useDispatch();
-    const ui = useSelector(selectUi);
-
     const params = useParams();
 
     if (params.latLng) {
@@ -31,10 +19,10 @@ const MainLayout = () => {
 
     return (
         <div className="relative">
-
             <Outlet />
 
             <MapWrapper />
+
         </div>
     )
 }
