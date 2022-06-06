ac0ce26ad16b83f25106ef0d033cb03a35feb0b1
LeoRaab <leopold.raab@gmail.com>
2022-06-06 14:34:15 +0200

    changed prop name type to colorScheme

--- src/hooks/use-color-scheme.ts
+++ src/hooks/use-color-scheme.ts
@@ -1,22 +1,22 @@
 import COLOR_SCHEME from "../types/ColorScheme";
 
 type ColorSchemeProps = {
-    type: COLOR_SCHEME | COLOR_SCHEME.ERROR
+    colorScheme: COLOR_SCHEME | COLOR_SCHEME.ERROR
 }
 
-const useColorScheme = ({ type }: ColorSchemeProps): {bgColor: string, textColor: string} => {
+const useColorScheme = ({ colorScheme }: ColorSchemeProps): {bgColor: string, textColor: string} => {
     let bgColor: string;
     let textColor: string;
 
-    switch (type) {
+    switch (colorScheme) {
         case COLOR_SCHEME.ERROR:
             bgColor = 'bg-red-400';
-            textColor = 'text-red-50';
+            textColor = 'text-white';
             break;
         case COLOR_SCHEME.SUCCESS:
         default:
             bgColor = 'bg-green-400';
-            textColor = 'text-green-50';
+            textColor = 'text-white';
             break;
     }
 
