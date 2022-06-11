26ddec6ca2741abfe5ae32eb605e53638247b8f3
LeoRaab <leopold.raab@gmail.com>
2022-06-11 17:43:16 +0200

    added url to venueImages type

--- src/types/VenueImage.ts
+++ src/types/VenueImage.ts
@@ -1,8 +1,9 @@
 interface VenueImage {
-    filename: string,
-    altText: string,
-    venueId?: string,
-    userId?: string
+    url: string,
+    id?: string,
+    altText?: string,    
+    userId?: string,
+    venueId?: string
 }
 
 export default VenueImage;
