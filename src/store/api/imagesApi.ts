c59b850742c3d2865eefdfa9161d0ad26baf648c
LeoRaab <leopold.raab@gmail.com>
2022-06-11 17:45:43 +0200

    add deleteImage endpoint

--- src/store/api/imagesApi.ts
+++ src/store/api/imagesApi.ts
@@ -1,6 +1,26 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react';
-import { RootState } from '../index';
 import VenueImage from '../../types/VenueImage';
+import { RootState } from '../index';
+
+interface DBVenueImage {
+    id: string,
+    filename: string,
+    altText: string,
+    user: string,
+    venue: string
+}
+
+const venueImagesFactory = (images: DBVenueImage[]): VenueImage[] => {
+    return images.map(image => {
+        return {
+            id: image.id,
+            url: 'http://localhost:5000/uploads/images/venues/' + image.venue + '/' + image.filename,
+            altText: image.altText,
+            venueId: image.venue,
+            userId: image.user
+        }
+    })
+}
 
 export const imagesApi = createApi({
     reducerPath: 'imagesApi',
@@ -18,15 +38,20 @@ export const imagesApi = createApi({
     endpoints: builder => ({
         getImagesByVenue: builder.query<VenueImage[], string>({
             query: (venueId) => ({ url: `/venue/${venueId}` }),
-            transformResponse: (responseData: any) => responseData.images,
+            transformResponse: (responseData: {images: DBVenueImage[]}) => venueImagesFactory(responseData.images),
             providesTags: ['Images']
         }),
-        getImagesByUser: builder.query<VenueImage[], string>({
-            query: (userId) => ({ url: `/${userId}` }),
-            transformResponse: (responseData: any) => responseData.images,
+        getImagesByUser: builder.query<VenueImage[], void>({
+            query: () => ({ url: `/user` }),
+            transformResponse: (responseData: {images: DBVenueImage[]}) => venueImagesFactory(responseData.images),
             providesTags: ['Images']
         }),
-        addImages: builder.mutation<{ message: string }, { venueId: string, uploadImages: FormData }>({
+        getImagesByVenueAndUser: builder.query<VenueImage[], string>({
+            query: (venueId) => ({ url: `/venue/${venueId}` }),
+            transformResponse: (responseData: { images: DBVenueImage[] }) => venueImagesFactory(responseData.images),
+            providesTags: ['Images']
+        }),
+        uploadImages: builder.mutation<{ message: string }, { venueId: string, uploadImages: FormData }>({
             query: ({ venueId, uploadImages }) => ({
                 url: venueId,
                 method: 'POST',
@@ -34,6 +59,13 @@ export const imagesApi = createApi({
             }),
             invalidatesTags: ['Images']
         }),
+        deleteImage: builder.mutation<{ message: string }, string>({
+            query: (imageId) => ({
+                url: imageId,
+                method: 'DELETE'
+            }),
+            invalidatesTags: ['Images']
+        })
         /*
         updateComment: builder.mutation<{ message: string }, { commentId: string, comment: string }>({
             query: ({ commentId, comment }) => ({
@@ -56,5 +88,8 @@ export const imagesApi = createApi({
 
 export const {
     useGetImagesByVenueQuery,
-    useAddImagesMutation
+    useGetImagesByUserQuery,
+    useGetImagesByVenueAndUserQuery,
+    useUploadImagesMutation,
+    useDeleteImageMutation
 } = imagesApi;
