29d13481c919495d919aace7081fc78c04e19156
LeoRaab <leopold.raab@gmail.com>
2022-06-12 10:56:31 +0200

    moved to features folder

---
+++ src/features/ImageManger/Thumbnail.tsx
@@ -0,0 +1,24 @@
+import Icon from '../../components/UI/Icon';
+import { ICONS } from '../../constants/Icons';
+import VenueImage from '../../types/VenueImage';
+
+type ThumbnailProps = {
+    image: VenueImage,
+    id: number,
+    onThumbnailClick?: (id: number) => void;
+}
+
+const Thumbnail = ({ image, id, onThumbnailClick }: ThumbnailProps) => {
+    return (
+        <div className="mr-1 mt-2 relative">
+            {onThumbnailClick &&
+                <div className="absolute z-800 top-0 right-0 bg-white/90 rounded text-red-500 cursor-pointer" onClick={() => onThumbnailClick(id)}>
+                    <Icon icon={ICONS.CLOSE} />
+                </div>
+            }
+            <img src={image.url} alt={image.altText || image.url} className="w-40" />
+        </div>
+    )
+}
+
+export default Thumbnail;
