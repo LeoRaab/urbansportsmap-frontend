2733802e4117ac6bb6787e57c19afd245dc58585
LeoRaab <leopold.raab@gmail.com>
2022-06-14 12:08:11 +0200

    changed design of GraphicMessage

--- src/components/UI/GraphicMessage.tsx
+++ src/components/UI/GraphicMessage.tsx
@@ -14,9 +14,9 @@ type GraphicMessageProps = {
 const GraphicMessage = ({illustration, title, text, link}: GraphicMessageProps) => {
 
     return (
-        <div className="mt-4">
-            <img src={illustration} alt={text} className=""/>
-            <div className="px-2 mt-8">
+        <div className="flex flex-col justify-center items-center mt-8">
+            <img src={illustration} alt={text} className="w-full"/>
+            <div className="px-2 mt-8 w-full">
                 <h3 className="text-xl">{title}</h3>
                 <p className="my-2">
                     {text}
