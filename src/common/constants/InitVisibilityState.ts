3f2ef04e279cc0cb8f318e3af9084aae3b800092
LeoRaab <leopold.raab@gmail.com>
2022-06-16 14:28:00 +0200

    removed not used constants

--- src/common/constants/InitVisibilityState.ts
+++
@@ -1,7 +0,0 @@
-import {VisibilityState} from '../types/VisibilityState';
-
-export const INIT_VISIBILITY_STATE: VisibilityState = {
-    showFilter: false,
-    showTeaser: false,
-    showSearchResults: false
-};
