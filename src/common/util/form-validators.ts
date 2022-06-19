09020a36f1a96ea6293d7ce43e73c78e5036ff63
LeoRaab <leopold.raab@gmail.com>
2022-06-19 18:33:48 +0200

    added confirm password validation

--- src/common/util/form-validators.ts
+++ src/common/util/form-validators.ts
@@ -1,53 +1,41 @@
-enum VALIDATOR_TYPE {
-    REQUIRE = "REQUIRE",
-    MINLENGTH = "MINLENGTH",
-    MAXLENGTH = "MAXLENGTH",
-    MIN = "MIN",
-    MAX = "MAX",
-    EMAIL = "EMAIL",
-    FILE = "FILE",
-}
-
 export interface Validator {
-    type: VALIDATOR_TYPE;
-    condition?: number;
+    condition?: number | string;
     isValid: (value: string) => boolean;
 }
 
 export const VALIDATOR_REQUIRE = (): Validator => ({
-    type: VALIDATOR_TYPE.REQUIRE,
     isValid: (value) => {
         return value.trim().length > 0;
     },
 });
 
 export const VALIDATOR_MINLENGTH = (condition: number): Validator => ({
-    type: VALIDATOR_TYPE.MINLENGTH,
     condition,
     isValid: (value) => value.trim().length >= condition,
 });
 
 export const VALIDATOR_MAXLENGTH = (condition: number): Validator => ({
-    type: VALIDATOR_TYPE.MAXLENGTH,
     condition,
     isValid: (value) => value.trim().length <= condition,
 });
 
 export const VALIDATOR_MIN = (condition: number): Validator => ({
-    type: VALIDATOR_TYPE.MIN,
     condition,
     isValid: (value) => +value! >= condition,
 });
 
 export const VALIDATOR_MAX = (condition: number): Validator => ({
-    type: VALIDATOR_TYPE.MAX,
     condition,
     isValid: (value) => +value <= condition,
 });
 
 export const VALIDATOR_EMAIL = (): Validator => ({
-    type: VALIDATOR_TYPE.EMAIL,
     isValid: (value) => /^\S+@\S+\.\S+$/.test(value),
+});
+
+export const VALIDATOR_CONFIRM_PASSWORD = (condition: string): Validator => ({
+    condition,
+    isValid: (value) => value === condition
 });
 
 export const validate = (value: string, validators: Validator[]) => {
