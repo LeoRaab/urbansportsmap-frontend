e8a1faafd68f11d450788ce96137673482515020
LeoRaab <leopold.raab@gmail.com>
2022-07-04 13:59:40 +0200

    added strings constant to centralize strings

---
+++ src/common/constants/strings.ts
@@ -0,0 +1,6 @@
+export const STRINGS = {
+    FAVORITES_ADD: 'Zu Favoriten hinzufügen',
+    FAVORITES_REMOVE: 'Von Favoriten entfernen',
+    IMAGES_EDIT: 'Bilder bearbeiten',
+    COMMENTS_ADD: 'Kommentar schreiben',
+}
