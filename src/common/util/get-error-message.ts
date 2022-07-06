b2fd70ce42c97d642d408f9da98e5b65bc869356
LeoRaab <leopold.raab@gmail.com>
2022-07-06 15:42:16 +0200

    added ErrorData interface

--- src/common/util/get-error-message.ts
+++ src/common/util/get-error-message.ts
@@ -2,9 +2,21 @@ import { SerializedError } from '@reduxjs/toolkit';
 import { FetchBaseQueryError } from '@reduxjs/toolkit/dist/query';
 import { STRINGS } from '../constants/strings';
 
+interface ErrorData {
+    message: string
+}
+
 const getErrorMessage = (error: FetchBaseQueryError | SerializedError): string => {
   if ('status' in error) {
-    return 'error' in error ? error.error : JSON.stringify(error.data);
+
+    if ('error' in error) {
+      return error.error
+    }
+
+    if ('data' in error) {
+      const errorData = error.data as ErrorData;
+      return errorData.message;
+    }
   }
 
   if ('message' in error && error.message) {
