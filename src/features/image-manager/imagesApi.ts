77f68d8e19d3cc5d8e3bb52eb4e1038579fc22dc
LeoRaab <leopold.raab@gmail.com>
2022-08-02 18:20:36 +0200

    changed response type

--- src/features/image-manager/imagesApi.ts
+++ src/features/image-manager/imagesApi.ts
@@ -1,41 +1,21 @@
 import apiSlice from '../../app/apiSlice';
 import VenueImage from '../../common/types/VenueImage';
 
-interface DBVenueImage {
-  id: string;
-  filename: string;
-  altText: string;
-  user: string;
-  venue: string;
-}
-
-const venueImagesFactory = (images: DBVenueImage[]): VenueImage[] => {
-  return images.map((image) => {
-    return {
-      id: image.id,
-      url: 'http://localhost:5000/uploads/images/venues/' + image.venue + '/' + image.filename,
-      altText: image.altText,
-      venueId: image.venue,
-      userId: image.user,
-    };
-  });
-};
-
 export const imagesApi = apiSlice.enhanceEndpoints({ addTagTypes: ['Images'] }).injectEndpoints({
   endpoints: (builder) => ({
     getImagesByVenue: builder.query<VenueImage[], string>({
       query: (venueId) => ({ url: `images/venue/${venueId}` }),
-      transformResponse: (responseData: { images: DBVenueImage[] }) => venueImagesFactory(responseData.images),
+      transformResponse: (responseData: { images: VenueImage[] }) => responseData.images,
       providesTags: ['Images'],
     }),
     getImagesByUser: builder.query<VenueImage[], void>({
       query: () => ({ url: `images/user` }),
-      transformResponse: (responseData: { images: DBVenueImage[] }) => venueImagesFactory(responseData.images),
+      transformResponse: (responseData: { images: VenueImage[] }) => responseData.images,
       providesTags: ['Images'],
     }),
     getImagesByVenueAndUser: builder.query<VenueImage[], string>({
       query: (venueId) => ({ url: `images/venue/${venueId}/user` }),
-      transformResponse: (responseData: { images: DBVenueImage[] }) => venueImagesFactory(responseData.images),
+      transformResponse: (responseData: { images: VenueImage[] }) => responseData.images,
       providesTags: ['Images'],
     }),
     uploadImages: builder.mutation<{ message: string }, { venueId: string; uploadImages: FormData }>({
